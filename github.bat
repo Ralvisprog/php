4fcab3aa9492c3de55635fa682759ec16bbefeb1
Ralvisprog <ralvis.oliveira@gmail.com>
2021-09-25 15:19:41 -0300

    Atualizado projeto pelo BAT

--- github.bat
+++ github.bat
@@ -1,6 +1,9 @@
 CD\
 CD xampp/htdocs
 git init
+git remote add origin https://github.com/Ralvisprog/php.git
+git config --global user.name "Ralvisprog"
+git config --global user.email "ralvis.oliveira@gmail.com
 git add --all
 git commit -m "Atualizado projeto pelo BAT"
 git push origin master
